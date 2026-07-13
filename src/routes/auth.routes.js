e5d6b24f8c39cc129ccfb14ded080d6ef62e6706
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2026-07-13 14:01:52 -0300

    feat: add authenticated middleware in logout endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/auth-controller');
-
+const { authenticateToken } = require('../middlewares/auth-middleware');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout', logout, authenticateToken);
 
 module.exports = router;
